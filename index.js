afbc2f70bcc24fb26414086915ed4e0a3a58b1e2
leon-do <leondo123@gmail.com>
2020-07-09 15:23:28 -0400

    unstoppabledomains/resolution

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const ipfs = require("ipfs");
 const app = express();
+const { Resolution } = require("@unstoppabledomains/resolution");
 const PORT = process.env.PORT || 5000;
 
-// start IPFS node
-let node;
-(async function start() {
-  node = await ipfs.create();
-})();
+const resolution = new Resolution({
+  blockchain: {
+    ens: { url: "https://mainnet.infura.io/v3" },
+    cns: { url: "https://mainnet.infura.io/v3" },
+  },
+});
 
 // listen
 app.get("*", async (req, res) => {
   // parse
-  const subdomain = req.subdomains.reverse().join(".");
+	const subdomain = req.subdomains.reverse().join(".").concat('.zil')
   try {
     // resolve dns
-    const path = await node.dns(subdomain);
+		const cid = await resolution.ipfsHash(subdomain);
     // redirect
-    return res.redirect(`http://gateway.ipfs.io${path}`);
+    return res.redirect(`http://gateway.ipfs.io/ipfs/${cid}`);
   } catch (error) {
     // this page
     res.sendFile(__dirname + "/public/index.html");
