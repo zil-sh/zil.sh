94fecac255221e92caff700d7bd72dcda50e6362
leon-do <leondo123@gmail.com>
2020-07-13 13:49:18 -0400

    resolve CID and http

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { Resolution } = require("@unstoppabledomains/resolution");
-var favicon = require('serve-favicon')
+var favicon = require("serve-favicon");
 const PORT = process.env.PORT || 5000;
 
 const resolution = new Resolution({
@@ -11,22 +11,28 @@ const resolution = new Resolution({
   },
 });
 
-app.use(favicon(__dirname + "/public/favicon.ico"))
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // listen
 app.get("*", async (req, res) => {
   // parse
   const subdomain = req.subdomains.reverse().join(".").concat(".zil");
-  try {
-    // resolve dns
-    const cid = await resolution.ipfsHash(subdomain);
-    // redirect
-    return res.redirect(`http://gateway.ipfs.io/ipfs/${cid}`);
-  } catch (error) {
-    // this page
-    res.sendFile(__dirname + "/public/index.html");
-  }
-});
 
+  // resolve dns. can either be IPFS CID or http URL
+  const cid = await resolution.ipfsHash(subdomain).catch((e) => false);
+  const url = await resolution.httpUrl(subdomain).catch((e) => false);
+
+  // redirect to IPFS gateway
+  if (cid) {
+    return res.redirect(`http://gateway.ipfs.io/ipfs/${cid}`);
+  }
+
+  // redirect to http url
+  if (url) {
+    return res.redirect(url);
+  }
+
+  return res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
